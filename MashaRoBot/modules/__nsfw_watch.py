4b31af516d7f621985b8cab4fec96bb04ac7835a
The Unknown <77438385+TheUnknownKanger@users.noreply.github.com>
2021-04-03 12:56:49 +0530

    Update __nsfw_watch.py

--- MashaRoBot/modules/__nsfw_watch.py
+++ MashaRoBot/modules/__nsfw_watch.py
@@ -111,7 +111,7 @@ async def ws(event):
 
 
 __help__ = """
-Masha can protect your group from NSFW senders
+Emcee can protect your group from NSFW senders
  ❍ /addnsfw*:* Adds The Group to nsfw Watch List
  ❍ /rmnsfw*:* Removes The Group From nsfw Watch List
 """
