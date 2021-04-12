d717e40b818a2a02aa7a5385a9d7b9bf2a32acac
Aquila-14 <78516399+Aquila-14@users.noreply.github.com>
2021-04-12 16:34:43 +0530

    Update _pyrosongs.py

--- MashaRoBot/modules/_pyrosongs.py
+++ MashaRoBot/modules/_pyrosongs.py
@@ -17,7 +17,7 @@ def time_to_seconds(time):
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
 
-@pbot.on_message(filters.command(['Audio']))
+@pbot.on_message(filters.command(['audio']))
 def song(client, message):
 
     user_id = message.from_user.id 
