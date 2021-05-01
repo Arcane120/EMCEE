ceed5109f1ad7685d0dd77a30136965ff4799c23
Aquila-14 <78516399+Aquila-14@users.noreply.github.com>
2021-05-02 00:02:16 +0530

    Update __init__.py

--- MashaRoBot/__init__.py
+++ MashaRoBot/__init__.py
@@ -198,7 +198,7 @@ updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("masha", API_ID, API_HASH)
 pbot = Client("mashapbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 mongo_client = MongoClient(MONGO_DB_URI)
-db = mongo_client.SaitamaRobot
+db = mongo_client.MashaRoBot
 arq = ARQ(ARQ_API)
 dispatcher = updater.dispatcher
 
